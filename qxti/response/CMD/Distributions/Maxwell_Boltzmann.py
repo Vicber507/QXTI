f08a23b260b4a8260321b997779c6b3a68480f05
ExMtz <erixmonteza07@gmail.com>
2026-05-27 23:02:47 -0500

    Update Maxwell_Boltzmann.py

--- qxti/response/CMD/Distributions/Maxwell_Boltzmann.py
+++ qxti/response/CMD/Distributions/Maxwell_Boltzmann.py
@@ -1,1 +1,11 @@
 #Contain the Maxwell-Boltzmann distribution function for classical particles.
+
+import numpy as np
+
+kB = 8.617333262e-5
+
+def maxwell_boltzmann(E, mu, T):
+
+    beta = 1.0 / (kB * T)
+
+    return np.exp(-beta * (E - mu))
