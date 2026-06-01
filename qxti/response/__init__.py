acf55afa2da2851909dcad21e7d4ddddc300af80
AssautJeridini <assautjeridini1120@gmail.com>
2026-05-31 20:40:44 -0500

    Pocoyo xd

--- qxti/response/__init__.py
+++ qxti/response/__init__.py
@@ -1,7 +1,7 @@
 from .cmd import CMD
 from .distributions import T1T2Relaxation, bose_einstein, fermi_dirac, full_occupation, maxwell_boltzmann, t1_t2_relaxation, valence_occupation
 from .xtp import XTP
-from .susceptibilityTensorCalculator import SusceptibilityTensorCalculator
+from .susceptibility_tensor_calculator import SusceptibilityTensorCalculator
 
 __all__ = [
     "CMD",
