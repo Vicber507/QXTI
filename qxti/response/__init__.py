3aaa423dc9a16191b0d5f8496198989442e9d735
AssautJeridini <assautjeridini1120@gmail.com>
2026-05-31 20:34:31 -0500

    Lo mismo

--- qxti/response/__init__.py
+++ qxti/response/__init__.py
@@ -1,10 +1,12 @@
 from .cmd import CMD
 from .distributions import T1T2Relaxation, bose_einstein, fermi_dirac, full_occupation, maxwell_boltzmann, t1_t2_relaxation, valence_occupation
 from .xtp import XTP
+from .susceptibilityTensorCalculator import SusceptibilityTensorCalculator
 
 __all__ = [
     "CMD",
     "XTP",
+    "SusceptibilityTensorCalculator",
     "T1T2Relaxation",
     "bose_einstein",
     "fermi_dirac",
